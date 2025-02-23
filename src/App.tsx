87259e6978d3643c463e2997be0131b14f942b50
fanlumaster <1730976608@qq.com>
2025-02-24 07:01:16 +0800

    config build rules

--- src/App.tsx
+++ src/App.tsx
@@ -36,13 +36,7 @@ function App() {
         <button onClick={onClick}>
           Click Me
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      input: {
+        popup: path.resolve(__dirname, "index.html"),
+        content: path.resolve(__dirname, "src/content/content.ts"),
+        background: path.resolve(__dirname, "src/background/background.ts")
+      },
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "chunks/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]"
+      }
+    }
+  }
 })
